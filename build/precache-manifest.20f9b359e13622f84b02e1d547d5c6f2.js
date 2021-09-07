f36d24341f16ead4b764b496f062b8e6538d72fd
Han Wang <hyw2@cornell.edu>
2021-09-06 22:05:37 -0400

    Update build files

---
+++ build/precache-manifest.20f9b359e13622f84b02e1d547d5c6f2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51661f84157a8de88cb5fbfa37b0e4cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d11ca5fcdd3c9832343",
+    "url": "/static/css/2.777731e7.chunk.css"
+  },
+  {
+    "revision": "fd236c5ba46130145948",
+    "url": "/static/css/main.51e9be65.chunk.css"
+  },
+  {
+    "revision": "3d11ca5fcdd3c9832343",
+    "url": "/static/js/2.181d9754.chunk.js"
+  },
+  {
+    "revision": "fd236c5ba46130145948",
+    "url": "/static/js/main.f951b19f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.38cc93aa3a01d2acc223ee30e498751a.js"
+  "/precache-manifest.20f9b359e13622f84b02e1d547d5c6f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
